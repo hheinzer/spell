e326b4e613b26e4b5d707deb6348443587d2dc06
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2023-04-05 15:45:13 +0200

    change hash function to fnv hash

--- src/strhash.c
+++ src/strhash.c
@@ -1,10 +1,29 @@
 #include "strhash.h"
 
+__attribute__((unused)) static size_t djb2(const char *str)
+{
+    // source: http://www.cse.yorku.ca/~oz/hash.html
+    size_t hash = 5381;
+    unsigned char c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+    }
+    return hash;
+}
+
+__attribute__((unused)) static size_t fnv(const char *str)
+{
+    // source: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+    size_t hash = 14695981039346656037ULL;
+    unsigned char c;
+    while ((c = *str++)) {
+        hash ^= c;
+        hash *= 1099511628211ULL;
+    }
+    return hash;
+}
+
 size_t strhash(const char *str)
 {
-    // djb2 hash function (http://www.cse.yorku.ca/~oz/hash.html)
-    size_t hash = 5381;
-    int c;
-    while ((c = *str++)) hash = ((hash << 5) + hash) + c; // hash * 33 + c
-    return hash;
+    return fnv(str);
 }
